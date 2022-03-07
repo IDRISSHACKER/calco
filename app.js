5ca11eb5109c4f6bb78bf083c7e7b9c57d3f9c3b
IDRISSHACKER <idrisscoder@gmail.com>
2022-03-07 23:16:00 +0100

    Prise en compte de plusieurs valeur et optimisation du code

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ let resultat = document.querySelector('.container-screen span')
 
 ecran.innerHTML = ""
 
-let error = () => { 
+let error = () => {
     ecran.innerText = ""
-    ecran.classList.add('error') 
+    ecran.classList.add('error')
 }
 let del_error = () => { ecran.classList.remove('error') }
 
@@ -17,135 +17,30 @@ const operation = ['+', '-', 'X', '/', '%']
 
 // Saisi de tous les elements sur l'ecran quand ils recevoint un click
 valeur.forEach(element => {
-    element.addEventListener('click', ()=>{
+    element.addEventListener('click', () => {
         del_error()
         ecran.append(element.innerHTML)
     })
 })
 
-egal.addEventListener('click', (e)=>{
-    if (ecran.innerHTML == ""){
+egal.addEventListener('click', (e) => {
+    if (ecran.innerHTML == "") {
         error()
         //return
     }
-    if (ecran.innerHTML !== ""){
-        resultat_nombre(ecran.innerHTML)
-        addition()
-        soustraction()
-        division()
-        multiplication()
+    if (ecran.innerHTML !== "") {
+        ecran.innerHTML = eval(ecran.innerHTML)
     }
 })
 
-// Fonction verifier si le resultat est un nombre
-let resultat_nombre = (nbre) => {
-    if (isNaN(parseFloat(nbre))) {
-        error()    
-    }
-}
-
-// let signe_consecutif = () => {
-//     operation.forEach(element => {
-//         let new_valeurs = ecran.innerHTML.split('')
-//         new_valeurs.forEach(k => {
-//             if (element == k) {
-//                 let positions = []
-//                 let position = new_valeurs.indexOf(k)
-//                 positions.push(position)
-//                 console.log(positions[2])
-//             }
-//         })
-//     })
-// }
-
-// Fonction pour gerer l'addition
-let addition = () => {
-    let new_valeurs = ecran.innerHTML.split('')
-    new_valeurs.forEach(element => {
-        if (element == "+") {
-            let partie1 = parseFloat(((new_valeurs.slice(0, new_valeurs.indexOf(element)))).join(''))
-            let partie2 = parseFloat((new_valeurs.slice((new_valeurs.indexOf(element))+1, new_valeurs.length+1)).join(''))
-            // console.log(partie1)
-            // console.log(partie2)
-            let nbre = partie1 + partie2
-            resultat_nombre(nbre)          
-            if (!isNaN(parseFloat(nbre))) {
-                ecran.innerHTML = nbre;   
-            }
-            index++
-        }
-    })
-}
-
-// Fonction pour gerer soustraction
-let soustraction = () => {
-    let new_valeurs = ecran.innerHTML.split('')
-    new_valeurs.forEach(element => {
-        if (element == "-") {
-            let partie1 = parseFloat(((new_valeurs.slice(0, new_valeurs.indexOf(element)))).join(''))
-            let partie2 = parseFloat((new_valeurs.slice((new_valeurs.indexOf(element))+1, new_valeurs.length+1)).join(''))
-            // console.log(partie1)
-            // console.log(partie2)
-            let nbre = partie1 - partie2
-            resultat_nombre(nbre)          
-            if (!isNaN(parseFloat(nbre))) {
-                ecran.innerHTML = nbre;   
-            }
-        }
-    })
-}
-
-// Fonction pour gerer division
-let division = () => {
-    let new_valeurs = ecran.innerHTML.split('')
-    new_valeurs.forEach(element => {
-        if (element == "/") {
-            let partie1 = parseFloat(((new_valeurs.slice(0, new_valeurs.indexOf(element)))).join(''))
-            let partie2 = parseFloat((new_valeurs.slice((new_valeurs.indexOf(element))+1, new_valeurs.length+1)).join(''))
-            // console.log(partie1)
-            // console.log(partie2)
-            let nbre = partie1 / partie2
-            resultat_nombre(nbre)          
-            if (!isNaN(parseFloat(nbre))) {
-                ecran.innerHTML = nbre;  
-            }
-            if (nbre == Infinity) {
-                ecran.innerHTML = ""
-                error()
-            }
-        }
-    })
-}
-
-// // Fonction pour gerer multiplication
-let multiplication = () => {
-    let new_valeurs = ecran.innerHTML.split('')
-    new_valeurs.forEach(element => {
-        if (element == "X") {
-            let partie1 = parseFloat(((new_valeurs.slice(0, new_valeurs.indexOf(element)))).join(''))
-            let partie2 = parseFloat((new_valeurs.slice((new_valeurs.indexOf(element))+1, new_valeurs.length+1)).join(''))
-            // console.log(partie1)
-            // console.log(partie2)
-            let nbre = partie1 * partie2
-            resultat_nombre(nbre)          
-            if (!isNaN(parseFloat(nbre))) {
-                ecran.innerHTML = nbre;   
-            }
-        }
-    })
-}
-
-
-// Effacer le dernier caractere
-effacer.addEventListener('click', (e)=>{
+effacer.addEventListener('click', (e) => {
     let nbre_valeurs = ecran.innerHTML.length
-    let liste_new_valeurs = ecran.innerHTML.split('').slice(0, nbre_valeurs-1)
+    let liste_new_valeurs = ecran.innerHTML.split('').slice(0, nbre_valeurs - 1)
     let new_valeurs = liste_new_valeurs.join('')
     ecran.innerHTML = new_valeurs
 })
 
-// Effacer toutes les valeurs saisies sur l'ecran
-clean.addEventListener('click', (e)=>{
+clean.addEventListener('click', (e) => {
     del_error()
     ecran.innerHTML = ""
 })
